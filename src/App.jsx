9492235340b6c489ed600198b881b43415a3b01c
phuctdang <phuctrungdang@gmail.com>
2024-03-25 14:34:55 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import './App.css'
+import { puppyList } from './data.js'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies)
 
   return (
     <>
-     
+     {
+      puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+      })
+     }
     </>
   )
 }

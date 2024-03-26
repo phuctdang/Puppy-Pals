f98b88a26cc99f2d498ee5b48640e1c482a0f3b3
phuctdang <phuctrungdang@gmail.com>
2024-03-26 00:13:41 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,27 @@ import { puppyList } from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null);
+  const featurePup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featurePup)
 
   return (
     <>
      {
       puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>
+        return <p onClick = {()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
       })
      }
+
+     {featPupId && (
+      <div>
+        <h2>{featurePup.name}</h2>
+        <ul>
+          <li>Age: {featurePup.age}</li>
+          <li>Email: {featurePup.email}</li>
+        </ul>
+      </div>
+     )}
     </>
   )
 }
